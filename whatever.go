0dc3bb41dc87c5f3a76b65182b5daa3f6b3c9621
Troy Cox <tcox@mtmrecognition.com>
2015-09-22 22:25:52 -0500

    imported graceful

--- whatever.go
+++ whatever.go
@@ -3,10 +3,12 @@ package main
 import (
 	"net/http"
 	. "github.com/troy-work/whateversite/components"
+	. "gopkg.in/tylerb/graceful.v1"
 	"log"
+	"time"
 )
 
-func redirHttps(w http.ResponseWriter, req *http.Request) {
+func redirectHttps(w http.ResponseWriter, req *http.Request) {
 	log.Printf("redirecting to https")
 	http.Redirect(w, req, "https://localhost:1443"+req.RequestURI, http.StatusMovedPermanently)
 }
@@ -14,18 +16,32 @@ func redirHttps(w http.ResponseWriter, req *http.Request) {
 
 func main() {
 
-	http.HandleFunc("/", DefaultHandler)
-	http.HandleFunc("/view/", MakeHandler(ViewHandler))
-	http.HandleFunc("/edit/", MakeHandler(EditHandler))
-	http.HandleFunc("/save/", MakeHandler(SaveHandler))
+	mux := http.NewServeMux()
 
-	go func(){
-		err := http.ListenAndServeTLS(":1443", "cert/server.pem", "cert/server.key", nil)
-		if (err != nil){
-			log.Fatal("ListenAndServe", err)
-		}
-	}()
+	mux.HandleFunc("/", DefaultHandler)
+	mux.HandleFunc("/view/", MakeHandler(ViewHandler))
+	mux.HandleFunc("/edit/", MakeHandler(EditHandler))
+	mux.HandleFunc("/save/", MakeHandler(SaveHandler))
 
-	http.ListenAndServe(":8080", http.HandlerFunc(redirHttps))
+	srvTls :=  &http.Server{
+		Addr: ":1443",
+		Handler: mux,
+	}
+
+	err := ListenAndServeTLS(srvTls, "cert/server.pem", "cert/server.key", time.Second)
+	if (err != nil){
+		log.Fatal("ListenAndServe", err)
+	}
+
+	mux2 := http.NewServeMux()
+	mux2.Handle("*",http.HandlerFunc(redirectHttps))
+
+	srv :=  &http.Server{
+		Addr: ":1443",
+		Handler: mux2,
+	}
+
+	//DON'T USE ListenAndServe like this. You can't shut it down
+	ListenAndServe(srv, time.Second)
 
 }
